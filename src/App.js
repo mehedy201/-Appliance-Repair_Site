733e75b21e9c3be9dac233bbe976838e2473e5f8
mehedy201 <mehedy643@gmail.com>
2022-11-25 11:55:09 +0600

    create responsive navigation menu and create some black page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import ContactUs from './Pages/ContactUS/ContactUs';
+import Home from './Pages/Home/Home';
+import Services from './Pages/Services/Services';
+import NavigationMenu from './Pages/Sheard/NavigaionMenu/NavigationMenu';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+    {/* Header ------------Start ---- */}
+      <header>
+        <NavigationMenu></NavigationMenu>
+      </header>
+    {/* Header ------------End ---- */}
+     {/* Routes ------------------------------------------------------------------------------------- Start ----- */}
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/home' element={<Home/>}></Route>
+        <Route path='/services' element={<Services/>}></Route>
+        <Route path='/contact-us' element={<ContactUs/>}></Route>
+      </Routes>
+     {/* Routes --------------------------------------------------------------------------------------- End ----- */}
+     {/* Footer ----------- Start ----- */}
+     
+     {/* Footer ----------- End ----- */}
+    </>
   );
 }
 
---
+++ src/Pages/Sheard/NavigaionMenu/NavigationMenu.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import logo from '../../../images/logo/wide logo.png'
+import './NaviagtionMenu.css';
+import { ImWrench } from 'react-icons/im';
+import { ImPhone } from 'react-icons/im';
+
+
+const NavigationMenu = () => {
+    // UseNavigate import from React --------------------------------------------
+    const navigate = useNavigate();
+    // Handle Logo Click --------------------------------------------------------
+    const logoClick = () =>{
+        navigate('/')
+    }
+
+    return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <div className="container">
+                    <img onClick={logoClick} className='website_logo' src={logo} alt="Appliance Repair 101" />
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                  <div className="d-flex ms-auto">
+                    <Link className='menu_text fs-5' to={'/services'}><ImWrench className='menu_text_icon'/>Services</Link>
+                    <Link className='menu_text fs-5' to={'/contact-us'}><ImPhone className='menu_text_icon'/>Contact Us</Link>
+                  </div>
+                </div>
+              </div>
+            </nav>
+    );
+};
+
+export default NavigationMenu;
