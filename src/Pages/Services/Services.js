6f0cf071b7a07e856d376b977e32b88a3d79b322
mehedy201 <mehedy643@gmail.com>
2022-11-29 11:01:00 +0600

    completed services page hero section

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './Services.css'
 
 const Services = () => {
     return (
-        <div>
-            <h1>Services</h1>
-        </div>
+        <>
+            <div className='services_page_background py-3'>
+                <div className="container py-5">
+                    <h5 className='fs-2 for_font_family text-white'>Services</h5>
+                    <p className='text-white'>We always strive to provide high quality service to the client.</p>
+                </div>
+            </div>
+            <div className="container py-5">
+                <p className='fs-3 fw-bold for_font_family'>We offer comprehensive diagnostic and repair services for <span style={{color: '#0073dd'}} className=' for_font_family'>any</span> home appliance</p>
+                <ul>
+                    <li className='fs-5 fw-bold'>Refrigerators</li>
+                    <li className='fs-5 fw-bold'>Washing Machines</li>
+                    <li className='fs-5 fw-bold'>Ovens</li>
+                    <li className='fs-5 fw-bold'>Dishwashers</li>
+                    <li className='fs-5 fw-bold'>Freezers</li>
+                    <li className='fs-5 fw-bold'>Dryers</li>
+                    <li className='fs-5 fw-bold'>Icemakers</li>
+                </ul>
+            </div>
+        </>
     );
 };
 
